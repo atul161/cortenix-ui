3d93342d03474a38f2ce9299130477d62bf75a90
rishabhsaini619 <myemail>
2025-01-04 11:38:41 +0530

    fixed main conatiner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,45 +8,45 @@ import OurClients from "@/components/Section/OurClients/OurClients";
 import OurClientsMobile from "@/components/Section/OurClients/OurClientsMobile";
 import CuttingEdge from "@/components/Section/CuttingEdge/CuttingEdge";
 import CuttingEdgeMobile from "@/components/Section/CuttingEdge/CuttingEdgeMobile";
+import GenAI from "@/components/Section/GenAI/GenAI";
 
 export default function Home() {
-    // Define media query breakpoints
-    const isMobileOrTablet = useMediaQuery({ query: "(max-width: 900px)" });
+  // Define media query breakpoints
+  const isMobileOrTablet = useMediaQuery({ query: "(max-width: 900px)" });
 
-    return (
-        <div className="min-h-screen bg-gray-100">
-            <HeaderComponent/>
-            <main className="container flex flex-col max-w-[100vw] overflow-hidden">
-                {isMobileOrTablet ? (
-                    <div id="HeroSectionMobile" className="relative">
-                        <HeroSectionMobile/>
-                    </div>
-                ) : (
-                    <div id="HeroSection" className="relative">
-                        <HeroSection/>
-                    </div>
-                )}
-                {isMobileOrTablet ? (
-                    <div id="OurClientsMobile" className="relative">
-                        <OurClientsMobile/>
-                    </div>
-                ) : (
-                    <div id="OurClients" className="relative">
-                        <OurClients/>
-                    </div>
-                )}
-                {isMobileOrTablet ? (
-                    <div id="CuttingEdgeMobile" className="relative">
-                        <CuttingEdgeMobile/>
-                    </div>
-                ) : (
-                    <div id="CuttingEdge" className="relative">
-                        <CuttingEdge/>
-                    </div>
-                )}
-
-            </main>
-        </div>
-
-    );
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <HeaderComponent />
+      <main className=" flex flex-col">
+        {isMobileOrTablet ? (
+          <div id="HeroSectionMobile" className="relative">
+            <HeroSectionMobile />
+          </div>
+        ) : (
+          <div id="HeroSection" className="relative">
+            <HeroSection />
+          </div>
+        )}
+        {isMobileOrTablet ? (
+          <div id="OurClientsMobile" className="relative">
+            <OurClientsMobile />
+          </div>
+        ) : (
+          <div id="OurClients" className="relative">
+            <OurClients />
+          </div>
+        )}
+        {isMobileOrTablet ? (
+          <div id="CuttingEdgeMobile" className="relative">
+            <CuttingEdgeMobile />
+          </div>
+        ) : (
+          <div id="CuttingEdge" className="relative">
+            <CuttingEdge />
+          </div>
+        )}
+        <GenAI />
+      </main>
+    </div>
+  );
 }
