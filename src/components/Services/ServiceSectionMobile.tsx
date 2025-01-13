8b1d6754c0a61f65568ec5d02dba7e21119e9cf1
atul161 <atulanand199898dbg@gmail.com>
2025-01-13 20:53:42 +0530

    Added Fixes

--- src/components/Services/ServiceSectionMobile.tsx
+++ src/components/Services/ServiceSectionMobile.tsx
@@ -12,42 +12,42 @@ const servicesData = [
         description:
             'Revolutionize your industry with tailored AI solutions that address specific challenges, streamline processes, and enhance decision-making.',
         link: '#',
-        image: '/images/ServicesPhotos/ai-vertical.png',
+        image: '/Images/ServicesPhotos/ai-vertical.png',
     },
     {
         title: 'Cybersecurity',
         description:
             'Secure your enterprise with cutting-edge cybersecurity services. From threat detection to vulnerability management, we protect your digital assets.',
         link: '#',
-        image: '/images/ServicesPhotos/cyber-security.png',
+        image: '/Images/ServicesPhotos/cyber-security.png',
     },
     {
         title: 'UX Design',
         description:
             'Deliver exceptional user experiences with our innovative UX design services. We combine creativity and functionality to design intuitive interfaces.',
         link: '#',
-        image: '/images/ServicesPhotos/ux-designer.png',
+        image: '/Images/ServicesPhotos/ux-designer.png',
     },
     {
         title: 'Salesforce Services',
         description:
             'Unlock the power of Salesforce with our customized solutions. From implementation to optimization, we help you leverage Salesforce to drive sales.',
         link: '#',
-        image: '/images/ServicesPhotos/salesforce-services.png',
+        image: '/Images/ServicesPhotos/salesforce-services.png',
     },
     {
         title: 'Full-Stack Development',
         description:
             'Our full-stack developers excel in MERN stack, seamlessly combining MongoDB, Express.js, React, and Node.js to create high-performance web applications.',
         link: '#',
-        image: '/images/ServicesPhotos/full-stack-development.png',
+        image: '/Images/ServicesPhotos/full-stack-development.png',
     },
     {
         title: 'Digital Product Development',
         description:
             'From ideation to deployment, we provide end-to-end digital product development services, leveraging the latest technologies to bring your ideas to life.',
         link: '#',
-        image: '/images/ServicesPhotos/digital-product-development.png',
+        image: '/Images/ServicesPhotos/digital-product-development.png',
     },
 ];
 const ServiceSectionMobile: React.FC = () => {
